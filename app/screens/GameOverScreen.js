6ffdb97bb0efa7e7e2b252afff464216baac91f4
Volodymyr Melnyk <volodymyr.melnyk@firstbridge.io>
2019-12-08 18:26:46 +0200

    add local fonts and images

--- app/screens/GameOverScreen.js
+++ app/screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 const GameOverScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Game is Over!</Text>
+      <View style={styles.imageContainer}>
+        <Image source={require('../assets/img/success.png')} style={styles.image} resizeMode='cover'/>
+      </View>
       <Text>Number of rounds: {props.roundsNumber}</Text>
       <Text>Number was: {props.userNumber}</Text>
       <Button title="NEW GAME" onPress={props.onRestart} />
@@ -17,6 +20,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageContainer : {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+    borderWidth: 2,
+    borderColor: '#000',
+    overflow: 'hidden',
+    marginVertical: 30
+  },
+  image: {
+    width: '100%',
+    height: '100%'
   }
 });
 
